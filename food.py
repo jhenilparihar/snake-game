4784244df142fd9eb8248c681aa73fd16ce7c179
Jhenil Parihar <jhenilparihar@gmail.com>
2021-08-20 22:00:52 +0530

    Update food.py

--- food.py
+++ food.py
@@ -8,7 +8,7 @@ class Food(Turtle):
         self.shape('circle')
         self.penup()
         self.shapesize(stretch_wid=0.5, stretch_len=0.5)
-        self.color("red")
+        self.color("green")
         self.speed("fastest")
         self.refresh()
 
