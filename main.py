8c49a33632ec756a8ef23b0b07e557acb617f01e
Jhenil Parihar <jhenilparihar@gmail.com>
2021-09-09 22:59:27 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from tkinter import messagebox
 from turtle import Screen
 import time
 from snake import Snake
@@ -45,12 +46,21 @@ while game_is_on:
     x = snake.head.xcor()
     y = snake.head.ycor()
     if x > 270 or x < -270 or y > 270 or y < -270:
-        score.game_over()
-        game_is_on = False
-    # Detect collision with tail.
+        if not messagebox.askyesno("Game Over!", "Do you want to play again?"):
+            game_is_on = False
+            score.game_over()
+        else:
+            score.reset_()
+            snake.reset()
 
+    # Detect collision with tail.
     for i in snake.snake[1:]:
         if snake.head.distance(i) < 5:
-            score.game_over()
-            game_is_on = False
+            if not messagebox.askyesno("Game Over!", "Do you want to play again?"):
+                game_is_on = False
+                score.game_over()
+            else:
+                score.reset_()
+                snake.reset()
+
 screen.exitonclick()
